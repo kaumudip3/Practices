9a473a94fd98b79d239757553b64e7a64dd2fd4b
kaumudip3 <59964017+kaumudip3@users.noreply.github.com>
2020-08-13 19:51:42 -0400

    Create sortLinkedListsofZeroOnetwo.cpp

---
+++ LinkedLists/sortLinkedListsofZeroOnetwo.cpp
@@ -0,0 +1,8 @@
+//Input: 1 -> 1 -> 2 -> 0 -> 2 -> 0 -> 1 -> NULL
+// Output: 0 -> 0 -> 1 -> 1 -> 1 -> 2 -> 2 -> NULL
+
+
+/*Solution
+- Traverse the list to get frequencies of 0, 1 and 2
+- rewrite existing likedlist in the same order as freq.
+*/
