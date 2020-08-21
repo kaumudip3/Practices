47ee4899e368874d9ad4ddb597f84eba178f1630
kaumudip3 <59964017+kaumudip3@users.noreply.github.com>
2020-08-21 19:44:16 -0400

    Create permute.cpp

---
+++ Backtracking/permute.cpp
@@ -0,0 +1,42 @@
+
+/*
+Input: [1,2,3]
+Output:
+[
+  [1,2,3],
+  [1,3,2],
+  [2,1,3],
+  [2,3,1],
+  [3,1,2],
+  [3,2,1]
+]
+
+*/
+
+public:
+   
+    
+    // permute num[begin..end]
+    // invariant: num[0..begin-1] have been fixed/permuted
+	void permuteRecursive(vector<int> &num, int begin, vector<vector<int> > &result)	{
+		if (begin >= num.size()) {
+		    // one permutation instance
+		    result.push_back(num);
+		    return;
+		}
+		
+		for (int i = begin; i < num.size(); i++) {
+		    swap(num[begin], num[i]);
+		    permuteRecursive(num, begin + 1, result);
+		    // reset
+		    swap(num[begin], num[i]);
+		}
+    }
+    
+     vector<vector<int>> permute(vector<int>& nums) {
+      vector<vector<int> > result;
+	    
+	    permuteRecursive(nums, 0, result);
+	    return result;
+    }
+};
